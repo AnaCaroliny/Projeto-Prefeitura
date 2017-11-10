3e59166b671c465984176c770d84866426089065
Emerson Flying <32183184+emersonflying@users.noreply.github.com>
2017-11-10 15:03:48 -0200

    codigo fonte do projeto
    primeiro commit :+1:

---
+++ Portal Cidadão Prefeitura/insert.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+	<title>Minha primeira página PHP</title>
+	</head>
+	<body>
+
+    <?php
+include_once("conexao.html");
+
+$nome($_POST['Name']);
+
+
+?>
+
+	</body>
+	</html>
+
+    
+
+
+
